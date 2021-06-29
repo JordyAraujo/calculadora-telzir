26776b6e2ff41282057ecd876ea2d3aa36386539
Jordy Araújo de Alcântara <jordyaraujo@outlook.com>
2021-06-29 07:55:52 -0300

    Update template.php

--- Views/template.php
+++ Views/template.php
@@ -19,7 +19,6 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>PHP_base</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="robots" content="all,follow">
