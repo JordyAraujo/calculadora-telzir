833dfb8ecf7e836d2f82c8b8a14b6ad0c8e4ce3e
JordyAraujo <jordyaraujo@outlook.com>
2021-07-02 08:59:26 -0300

    404

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -2,22 +2,12 @@
 namespace Controllers;
 
 use \Core\Controller;
-// use \Models\Exemplo;
 
 class HomeController extends Controller
 {
-    // private $exemplo;
-
-    // public function __construct()
-    // {
-    //     $this->exemplo = new Exemplo();
-    // }
-
     public function index()
     {
         $array = array();
-
-        // $array['exmplo'] = $this->exemplo->getAll();
 
         $this->loadTemplate('home', $array);
     }
--- Controllers/NotfoundController.php
+++ Controllers/NotfoundController.php
@@ -4,9 +4,7 @@ namespace Controllers;
 use \Core\Controller;
 
 class NotfoundController extends Controller {
-
 	public function index() {
 		$this->loadView('404', array());
 	}
-
 }
